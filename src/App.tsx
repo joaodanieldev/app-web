02d434ea2256b5a549b9422523423fbc9e969ac2
Joao Daniel <joaodanieldev@gmail.com>
2021-05-30 16:06:20 -0300

    layout pagina inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './assets/styles/global.css';
+import Landgin from './pages/Landing';
+
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hellor world</h1>
-    </div>
+    <Landgin />
   );
 }
 
